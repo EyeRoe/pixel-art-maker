c0136d79067269847cc68aae4b5a53b4c0ed3f15
Allie <EyeRoe@users.noreply.github.com>
2017-12-04 11:50:18 -0800

    met basic requirement

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
   var canvas = document.querySelector('.canvas');
+  var palette = document.querySelector('.palette');
+  var color = '';
+  var backgroundColor = "background-color";
   console.log("yes hello");
-  
-  canvas.addEventListener("click", function(event){
-    var selectedPixel = event.target;
-    selectedPixel.className += " red";
-  })
+
+  canvas.addEventListener("click", paint, true);
+
+  palette.addEventListener("click", selectColor, true);
+
+
+  function selectColor(event) {
+    console.log('selected')
+    color = event.target.style.backgroundColor
+    //also update the color selected section.
+  }
+  function paint(event) {
+    console.log('it should change color')
+    var selectedPixel = event.target
+    selectedPixel.style.backgroundColor = color;
+    event.stopPropagation();
+  }
 })
+
+// canvas.addEventListener("click", function(event){
+//   var selectedPixel = event.target;
+//   selectedPixel.className += " red";
+// })
